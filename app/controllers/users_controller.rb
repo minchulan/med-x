d38d4419e518f5db7aab1afbbd04080e59131c47
Minchul An <itsminchul@gmail.com>
2023-10-18 13:29:16 -0600

    updates to users controller and user model, has one attached image.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,39 +1,46 @@
 class UsersController < ApplicationController
-    before_action :find_user, only: [:show, :update, :destroy]
-    skip_before_action :authorize_user, only: [:create]
-  
-    # GET "/users"
-    def index  
-        users = User.all.with_attached_image 
-        render json: users 
-    end  
+  before_action :find_user, only: [:show, :update, :destroy]
+  skip_before_action :authorize_user, only: [:create]
 
-    # GET "/me"
-    def show 
-        render json: @user, status: :ok 
-    end 
+  # GET "/users"
+  def index
+    users = User.all.with_attached_image
+    render json: users
+  end
 
-    # POST "/signup"
-    def create
+  # GET "/me"
+  def show
+    render json: @user, status: :ok
+  end
+
+  # POST "/signup"
+  def create
     new_user = User.new(user_params)
     if new_user.save
-        session[:user_id] = new_user.id
-        render json: new_user, status: :ok
+      session[:user_id] = new_user.id
+      render json: new_user, status: :created
     else
-        render json: { errors: new_user.errors.full_messages }, status: :unprocessable_entity
+      render json: { errors: new_user.errors.full_messages }, status: :unprocessable_entity
     end
+  end
+
+  # PUT "/users/:id/update_image"
+  def update_image
+    if current_user.update(image: params[:image])
+      render json: current_user, status: :ok
+    else
+      render json: { errors: current_user.errors.full_messages }, status: :unprocessable_entity
     end
+  end
 
+  private
 
-    private 
+  def user_params
+    params.permit(:username, :email, :password, :image)
+  end
 
-    def user_params 
-        params.permit(:username, :email, :password, :image)
-    end
-
-
-    def find_user 
-        @user = current_user
-        render_not_found unless @user 
-    end 
+  def find_user
+    @user = current_user
+    render_not_found unless @user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,20 @@
 Rails.application.routes.draw do
-  
-  resources :users, only: [:index, :show] # show action for user profiles 
+  resources :users, only: [:index, :show] do
+    # PUT "/users/:id/update_image"
+    member do
+      put 'update_image'
+    end
+  end
 
-  resources :posts do 
-    resources :comments, shallow: true 
-    resources :likes, only: [:create, :destroy] # Allow only create and destroy actions for likes
-  end 
-
+  resources :posts do
+    resources :comments, shallow: true
+    resources :likes, only: [:create, :destroy]
+  end
 
   patch "/posts/:post_id/comments/:id", to: "comments#update"
-
   get "/users/:user_id/posts", to: "posts#index"
 
-
-  # signup 
+  # signup
   post "/signup", to: "users#create"
 
   # login
@@ -22,9 +23,8 @@ Rails.application.routes.draw do
   # logout
   delete "/logout", to: "sessions#destroy"
 
-  # me 
+  # me
   get "/me", to: "users#show"
-
 
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
@@ -32,12 +32,19 @@ Rails.application.routes.draw do
 end
 
 
-#------------------------------------------------------
-# create a new comment in the context of a post. ["/posts/:post_id/comments"]
-# we want to see all the comments related to a single post. ["/posts/:post_id/comments"]
 
-# get "/users/:user_id/posts", to: "posts#index"
+  ## MEMBER
+    #  Member routes are routes that act on an individual member of the collection (in this case, a specific user). The put 'update_image' line creates a custom update_image route that can be accessed via a PUT request on a specific user's URL, like /users/:id/update_image.
 
-# shallow: true gives us the following routes:
-    # resources :comments, only: [:index, :create] <--nested
-    # resources :comments, only: [:show, :update, :destroy] <-- non-nested
+  ## PUT Request: 
+    #Purpose: The PUT request is used to update or create a resource at a specific URL.
+
+    #Idempotent: PUT requests are idempotent, meaning that making the same request multiple times will have the same effect as making it once. If you send a PUT request to a resource URL, it will replace the resource or create it if it doesn't exist.
+
+    #Full Update: When you send a PUT request, you typically send the entire updated resource to the server. This means if you have a user resource and you send a PUT request to update the user, you 
+
+
+  ## SHALLOW
+    # shallow: true gives us the following routes:
+      # resources :comments, only: [:index, :create] <--nested
+      # resources :comments, only: [:show, :update, :destroy] <-- non-nested
