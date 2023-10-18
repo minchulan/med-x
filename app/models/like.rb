3dcdd161d8647c527d746db5cdafa37cec2d25ca
Minchul An <itsminchul@gmail.com>
2023-10-18 12:45:42 -0600

    fixed conflicts.

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,7 @@ class Like < ApplicationRecord
   belongs_to :user 
   belongs_to :post, counter_cache: :likes_count
 
-  # validates_uniqueness_of :user_id, scope: :post_id
+  validates_uniqueness_of :user_id, scope: :post_id
 
 end
 
