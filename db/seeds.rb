c1f37397806f10bbb944e27df5d297784dc9947e
Minchul An <itsminchul@gmail.com>
2023-10-20 09:42:12 -0600

    using faker for seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,11 @@
+require 'faker'
+
 puts "Seeding data..."
 
+# Clear existing data 
+Like.destroy_all
+Comment.destroy_all
+Post.destroy_all
 User.destroy_all
 
 # Create users
@@ -7,16 +13,44 @@ admin = User.create(username: "admin", email: "admin@example.com", password: "pa
 minchul = User.create(username: "minchulan", email: "itsminchul@gmail.com", password: "password", bio: "Minchul An is a licensed pharmacist in NY and CO. Min is currently a flex student at Flatiron School's Software Engineering program. Min has a passion for building products that improve the patient care experience.")
 joey = User.create(username: "joey", email: "joey@gmail.com", password: "password", bio: "Joey is a test user without admin privileges. Joey is fictitious and has been created for testing purposes only.")
 
-# Create posts and assign them to users
-p1 = Post.create(title: "Post Title 1", content: "Some awesome lorem text lorem lorem text text", likes_count: 15, user: admin)
-p2 = Post.create(title: "Post Title 2", content: "Some awesome lorem text lorem lorem text text", likes_count: 5, user: minchul)
-p3 = Post.create(title: "Post Title 3", content: "Some awesome lorem text lorem lorem text text", likes_count: 10, user: joey)
+# Create more users
+10.times do
+  User.create(
+    username: Faker::Internet.unique.username,
+    email: Faker::Internet.unique.email,
+    password: "password",
+    bio: Faker::Lorem.sentence
+  )
+end
 
-# Create comments
-p1.comments.create(user: admin, content: "This was a great post! Thanks for sharing!")
-p2.comments.create(user: minchul, content: "This was a great post! Thanks for sharing!")
-p3.comments.create(user: joey, content: "This was a great post! Thanks for sharing!")
+users = User.all
+
+# Create posts and assign them to users
+5.times do
+  Post.create(
+    title: Faker::Lorem.sentence,
+    content: Faker::Lorem.paragraph,
+    likes_count: Faker::Number.between(from: 1, to: 50),
+    user: users.sample
+  )
+end
+
+posts = Post.all
+
+# Create comments and likes
+users.each do |user|
+  rand(1..2).times do
+    post = posts.sample
+    Comment.create(
+      user: user,
+      post: post,
+      content: Faker::Lorem.sentence
+    )
+    post.likes.create(user: user) if [true, false].sample # Randomly create likes for posts
+  end
+end
 
 puts "✅ Done seeding!"
+
 
 
