58856bf83cd3cc1e6cd33c7b79c6736b9db9871c
Minchul An <itsminchul@gmail.com>
2023-10-20 12:36:27 -0600

    cleaned up comments in my likeButton component and css.

--- client/src/LikeButton.js
+++ client/src/LikeButton.js
@@ -38,7 +38,7 @@ const LikeButton = ({ post, username }) => {
         } else {
           updatePostLikesCount(post.id, data.likes_count, username);
         }
-        setIsLiked(!isLiked); // Toggle isLiked state
+        setIsLiked(!isLiked);
       })
       .catch((error) => {
         console.error(error);
@@ -60,19 +60,3 @@ const LikeButton = ({ post, username }) => {
 };
 
 export default LikeButton;
-
-
-/*
-  To handle the like increment logic, you would typically do this on the server-side when a successful like request is made. The server should handle the logic of updating the likes count in the database and then send back the updated count to the client.
-
-  if `isLiked` is TRUE or FALSE: 
-    - If the post is already liked (isLiked is true), it sends a DELETE request to unlike the post.
-    - If the post is not liked (isLiked is false), it sends a POST request to like the post.
-
-  Updating Global State:
-    - It calls updateLikeCount(post.id, data.likes_count) from PostContext to update the like count for the specific post in the global state.
-
-    - It calls updateUserLikeCount(post.id) or updateUserUnlikeCount(post.id) from UserContext to update the currentUser state based on the postId.
-
-    - The LikeButton component updates both the posts state and the currentUser state based on the server response after 'liking' or 'un-liking' a post.
-*/
