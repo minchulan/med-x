c03678200977a9b99122753c8d157a0f26687248
Minchul An <itsminchul@gmail.com>
2024-03-05 17:47:26 -0700

    deployment fix again.

--- client/src/context/post.js
+++ client/src/context/post.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { createContext, useState, useEffect, useContext } from "react";
 import { UserContext } from "./user";
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDom from 'react-dom';
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
